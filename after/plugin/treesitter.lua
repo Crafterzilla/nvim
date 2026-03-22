d7d5becc91e919a23c7bed84c784c6deedbf43e7
Osmar Rojas <crafterzilla2.1@gmail.com>
2026-03-21 20:35:51 -0400

    Fix treesetter plugin

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,4 +1,29 @@
-require'nvim-treesitter.configs'.setup {
+-- require'nvim-treesitter.configs'.setup {
+--   -- A list of parser names, or "all" (the five listed parsers should always be installed)
+--   ensure_installed = { "c", "cpp", "python", "java", "bash", "lua", "vim", "vimdoc", "query", "json"},
+
+--   -- Install parsers synchronously (only applied to `ensure_installed`)
+--   sync_install = false,
+
+--   -- Automatically install missing parsers when entering buffer
+--   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+--   auto_install = true,
+
+--   highlight = {
+--     enable = true,
+
+--    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+--     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+--     -- Using this option may slow down your editor, and you may see some duplicate highlights.
+--     -- Instead of true it can also be a list of languages
+--     additional_vim_regex_highlighting = false,
+--   },
+-- }
+
+require('nvim-treesitter').setup {
+  -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+  install_dir = vim.fn.stdpath('data') .. '/site',
+
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   ensure_installed = { "c", "cpp", "python", "java", "bash", "lua", "vim", "vimdoc", "query", "json"},
 
@@ -17,5 +42,5 @@ require'nvim-treesitter.configs'.setup {
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
-  },
+    }
 }
